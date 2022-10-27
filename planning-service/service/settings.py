1c6c708f989f7c61c39745d799c08dec54afe2ce
davidwmartines <d5172@yahoo.com>
2022-10-27 16:53:26 -0500

    Use postgres.

--- planning-service/service/settings.py
+++ planning-service/service/settings.py
@@ -78,13 +78,17 @@ WSGI_APPLICATION = "service.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": env("PGHOST"),
+        "PORT": env("PGPORT", default=5432),
+        "NAME": env("PGDATABASE"),
+        "USER": env("PGUSER"),
+        "PASSWORD": env("PGPASSWORD"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
