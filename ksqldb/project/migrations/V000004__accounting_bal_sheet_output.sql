cbd07fdc200b95f93ffc6622b017c1a6f9d6a969
davidwmartines <d5172@yahoo.com>
2022-11-02 10:27:22 -0500

    Remove tie to physical schema id.

--- ksqldb/project/migrations/V000004__accounting_bal_sheet_output.sql
+++ ksqldb/project/migrations/V000004__accounting_bal_sheet_output.sql
@@ -1,14 +1,10 @@
 /*
 This creates a new stream by selecting from the input stream,
-projecting fields to the canonical schema defined in the schema registry,
+projecting fields to the canonical schema expected by consumers,
 and producing new messages into the public topic for other services
 to consume.
 The schema created in the projection must be compatible with the
 canonical schema for balance sheet events.
-The schema must already be registered for the subject (topic), and
-the VALUE_SCHEMA_ID references the desired version (default: 1).
-
-NOTE THIS IS NOT WORKING YET, probably because of https://github.com/confluentinc/ksql/issues/8919
 */
 
 SET 'auto.offset.reset' = 'earliest';
@@ -16,9 +12,7 @@ SET 'auto.offset.reset' = 'earliest';
 CREATE STREAM accounting_bal_sheet_output
 WITH (
     KAFKA_TOPIC = 'public_balance_sheet_entity_events',
-    PARTITIONS = 4,
-    VALUE_FORMAT = 'avro',
-    VALUE_SCHEMA_ID = 1
+    VALUE_FORMAT = 'avro'
 ) AS
     SELECT
         '1.0' AS `specversion`,
