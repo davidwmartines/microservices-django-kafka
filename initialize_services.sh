4f450720a708ed11782c2a26e67ce228cabcb5e0
davidwmartines <d5172@yahoo.com>
2022-11-03 13:19:07 -0500

    Fix dummy password.

--- initialize_services.sh
+++ initialize_services.sh
@@ -6,7 +6,7 @@ docker-compose run --rm \
 
 echo "Creating a superuser in the Accounting Service"
 docker-compose run --rm \
-    -e DJANGO_SUPERUSER_PASSWORD="P@$$w0rd1" \
+    -e DJANGO_SUPERUSER_PASSWORD=P@ssw0rd1 \
     -e DJANGO_SUPERUSER_USERNAME=root \
     -e DJANGO_SUPERUSER_EMAIL=root@root.com \
     accounting-service-web bash -c "./manage.py createsuperuser --no-input"
@@ -17,7 +17,7 @@ docker-compose run --rm \
 
 echo "Creating a superuser in the Person Service"
 docker-compose run --rm \
-    -e DJANGO_SUPERUSER_PASSWORD="P@$$w0rd1" \
+    -e DJANGO_SUPERUSER_PASSWORD=P@ssw0rd1 \
     -e DJANGO_SUPERUSER_USERNAME=root \
     -e DJANGO_SUPERUSER_EMAIL=root@root.com \
     person-service-web bash -c "./manage.py createsuperuser --no-input"
@@ -28,7 +28,7 @@ docker-compose run --rm \
 
 echo "Creating a superuser in the Planning Service"
 docker-compose run --rm \
-    -e DJANGO_SUPERUSER_PASSWORD="P@$$w0rd1" \
+    -e DJANGO_SUPERUSER_PASSWORD=P@ssw0rd1 \
     -e DJANGO_SUPERUSER_USERNAME=root \
     -e DJANGO_SUPERUSER_EMAIL=root@root.com \
     planning-service-web bash -c "./manage.py createsuperuser --no-input"
