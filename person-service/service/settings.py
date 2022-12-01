4be6009a815b4bab0edf72b2e25a1f7f6b2ac450
davidwmartines <d5172@yahoo.com>
2022-12-01 09:45:30 -0600

    Add health checks.

--- person-service/service/settings.py
+++ person-service/service/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "health_check",
     "person",
     "events",
 ]
--- planning-service/service/urls.py
+++ planning-service/service/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("health/", include("health_check.urls")),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
