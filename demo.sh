3a21ba14a272bb024861b0fd5c3d100540787d03
davidwmartines <d5172@yahoo.com>
2022-12-07 22:44:01 -0600

    Update demo script.

--- demo.sh
+++ demo.sh
@@ -8,7 +8,7 @@ set -v
 # create a person:
 person_id=$(curl -s -u $username:$password -H 'Accept: application/json; indent=4'\
     -H 'Content-Type: application/json' \
-    -d '{"first_name": "Bart", "last_name": "Simpson", "date_of_birth": "1981-01-01T00:00:00Z"}' \
+    -d '{"first_name": "Bart", "last_name": "Simpson", "date_of_birth": "1980-02-23T00:00:00Z"}' \
     http://localhost:8001/api/persons/ | jq -r ".id")
 
 echo ${person_id}
@@ -19,10 +19,9 @@ curl -s -u $username:$password -H 'Accept: application/json; indent=4'\
     -d '{"person_id": "'$person_id'",  "assets": 3340000, "liabilities": "537300"}' \
     http://localhost:8002/api/balance-sheets/
 
+# wait for eventual consistency...
+sleep 1
 
-
-
-
-# echo "list persons..."
-# curl -H 'Accept: application/json; indent=4' -u $username:$password \
-#     http://localhost:8001/api/persons/
+# get financial planning data
+curl -H 'Accept: application/json; indent=4' -u $username:$password \
+    http://localhost:8003/api/ratios/$person_id/
